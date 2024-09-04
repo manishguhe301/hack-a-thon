2be30ae7ed75ea894f5049048b44167e82798810
Manish Guhe <manishguhe301@gmail.com>
2024-09-04 17:59:38 +0530

    Added Data

---
+++ src/utils/data.js
@@ -0,0 +1,31 @@
+import benefit1 from '../assets/icons/benefit1.svg';
+import benefit2 from '../assets/icons/benefit2.svg';
+import benefit3 from '../assets/icons/benefit3.svg';
+import benefit4 from '../assets/icons/benefit4.svg';
+
+export const AIChallengesData = [
+  {
+    icon: benefit1,
+    title: 'Prove your skills',
+    description:
+      'Gain substantial experience by solving real-world problems and pit against others to come up with innovative solutions.',
+  },
+  {
+    icon: benefit2,
+    title: 'Learn from community',
+    description:
+      'One can look and analyze the solutions submitted by the other Data Scientists in the community and learn from them.',
+  },
+  {
+    icon: benefit3,
+    title: 'Challenge yourself',
+    description:
+      'There is nothing for you to lose by participating in a challenge. You can fail safe, learn out of the entire experience and bounce back harder.',
+  },
+  {
+    icon: benefit4,
+    title: 'Earn recognition',
+    description:
+      'You will stand out from the crowd if you do well in AI challenges, it not only helps you shine in the community but also earns rewards.',
+  },
+];
