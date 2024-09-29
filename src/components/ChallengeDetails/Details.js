f0c4858c729dbd2a679e73d4d33aacdb8aae0dfd
Manish Guhe <manishguhe301@gmail.com>
2024-09-29 21:11:38 +0530

    minor code update

--- src/components/ChallengeDetails/Details.js
+++ src/components/ChallengeDetails/Details.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import levelIcon from '../../assets/icons/carbon_skill-level-basic.svg';
 import clock from '../../assets/icons/clock.svg';
 import data from '../../utils/data.json';
@@ -23,6 +23,7 @@ import {
 const Details = () => {
   const { id } = useParams();
   const [challenge, setChallenge] = useState(null);
+  const navigate = useNavigate();
 
   const calculateStatus = (item) => {
     const date = new Date().getTime();
@@ -32,6 +33,22 @@ const Details = () => {
     if (start < date && end < date) return 'Past';
     if (start <= date && end > date) return 'Active';
     return 'Upcoming';
+  };
+
+  useEffect(() => {
+    const item = data.find((item) => item.id === Number(id));
+    if (item) {
+      setChallenge(item);
+    } else {
+      console.error('Challenge not found');
+    }
+  }, [id]);
+
+  const deleteChallenge = () => {
+    const updatedData = data.filter((item) => item.id !== Number(id));
+    console.log(updatedData);
+    setChallenge(null);
+    // navigate('/');
   };
 
   useEffect(() => {
@@ -57,28 +74,44 @@ const Details = () => {
 
   return (
     <ChallengeDetailsSection>
-      <ChallengeDetailsCtr>
-        <div>
-          <Status>
-            <ClockIcon src={clock} alt='clock__icon' />
-            {statusMessage} (India Standard Time)
-          </Status>
-          <ChallengeTitle>{name}</ChallengeTitle>
-          <ChallengeShortDesc>{tagLine}</ChallengeShortDesc>
-          <LevelIconWpr>
-            <LevelIcon src={levelIcon} alt='level__icon' />
-            <Level>{level}</Level>
-          </LevelIconWpr>
-        </div>
-      </ChallengeDetailsCtr>
-      <OverviewNavbar>
-        <OverviewHeading>Overview</OverviewHeading>
-        <div>
-          <EditBtn to={`/create-challenge/${id}`}>Edit</EditBtn>
-          <DelBtn to='/'>Delete</DelBtn>
-        </div>
-      </OverviewNavbar>
-      <FullDescription>{description}</FullDescription>
+      {challenge ? (
+        <>
+          <ChallengeDetailsCtr>
+            <div>
+              <Status>
+                <ClockIcon src={clock} alt='clock__icon' />
+                {statusMessage} (India Standard Time)
+              </Status>
+              <ChallengeTitle>{name}</ChallengeTitle>
+              <ChallengeShortDesc>{tagLine}</ChallengeShortDesc>
+              <LevelIconWpr>
+                <LevelIcon src={levelIcon} alt='level__icon' />
+                <Level>{level}</Level>
+              </LevelIconWpr>
+            </div>
+          </ChallengeDetailsCtr>
+          <OverviewNavbar>
+            <OverviewHeading>Overview</OverviewHeading>
+            <div>
+              <EditBtn to={`/create-challenge/${id}`}>Edit</EditBtn>
+              <DelBtn onClick={() => deleteChallenge(id)}>Delete</DelBtn>
+            </div>
+          </OverviewNavbar>
+          <FullDescription>{description}</FullDescription>
+        </>
+      ) : (
+        <p
+          style={{
+            textAlign: 'center',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          This Challenge Does Not Exists!
+        </p>
+      )}
     </ChallengeDetailsSection>
   );
 };
